b1e8d4e0b3f473036e9727de9a12f42afb02f162
kovelakondaaryan13 <kovelakondaaryan13@gmail.com>
2020-05-08 17:09:35 +0530

    add platform

--- Bird.js
+++ Bird.js
@@ -1,12 +1,19 @@
 class Bird extends BaseClass {
   constructor(x,y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
+    super(x,y,70,70);
+    
   }
 
   display() {
-    this.body.position.x = mouseX;
-    this.body.position.y = mouseY;
+   // this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    
     super.display();
+    
+    rectMode(CENTER);
+    strokeWeight(4);
+    stroke ("green");
+    fill(255);
   }
+  
 }
